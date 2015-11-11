9b733b2c5ef8a70a5d2a10ee7c17f02738ac56ae
unknown <mohisri@mohisri-dc7900.redmond.corp.microsoft.com>
2015-11-11 10:59:05 -0800

    More PowerShell updates

--- Set-AppSettings.ps1
+++ Set-AppSettings.ps1
@@ -9,11 +9,14 @@ $ezAuthSettings = Get-Content .\settings.ezauth.json | ConvertFrom-Json
 # MyDashAPI
 $appSettings = Get-Content (".\" + $balSite + "\secrets.json") | ConvertFrom-Json
 $appSettings | Add-Member -type NoteProperty -name DataAPIUrl -value ("https://" + $dalSite + ".azurewebsites.net") -force
-New-AzureRmResource -PropertyObject $appSettings -ResourceGroupName $rg -ResourceType Microsoft.Web/sites/config -ResourceName ($balSite + "/appsettings") -ApiVersion 2015-08-01 -Force
+## Call Set, converting appSettings from PSCustomObject to Hashtable
+Set-AzureRmWebApp -ResourceGroupName $rg -Name $balSite -AppSettings ($appSettings.psobject.properties | foreach -begin {$h=@{}} -process {$h."$($_.Name)" = $_.Value} -end {$h})
 $ezAuthSettings | Add-Member -type NoteProperty -name clientId -value $appSettings."ida:ClientId" -force
 $ezAuthSettings | Add-Member -type NoteProperty -name issuer -value $appSettings."ida:Authority" -force
+## Ideally would be Set, but there is a bug as of 11/11/2015 where -UsePatchSemantics still submits a GET. This is not allowed for /authsettings.
 New-AzureRmResource -PropertyObject $ezAuthSettings -ResourceGroupName $rg -ResourceType Microsoft.Web/sites/config -ResourceName ($balSite + "/authsettings") -ApiVersion 2015-08-01 -Force
 
-#MyDashDataAPI
+# MyDashDataAPI
 $ezAuthSettings | Add-Member -type NoteProperty -name clientId -value $appSettings."ida:Resource" -force
+## Ideally would be Set, but there is a bug as of 11/11/2015 where -UsePatchSemantics still submits a GET. This is not allowed for /authsettings.
 New-AzureRmResource -PropertyObject $ezAuthSettings -ResourceGroupName $rg -ResourceType Microsoft.Web/sites/config -ResourceName ($dalSite + "/authsettings") -ApiVersion 2015-08-01 -Force
